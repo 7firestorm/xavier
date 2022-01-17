f1406aa9c185a4cf1d624c689de3aebe0ab6eaad
Antonio Xavier <antonio.75bi@gmail.com>
2022-01-17 14:18:11 +0200

    Meu primeiro codigo com Laravel

---
+++ hcode-project/resources/views/construcao.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Pagina Em Manutenção</title>
+
+        <!-- Fonts -->
+        <style>
+            h1 {text-align: center;}
+            p {text-align: center;}
+            div {text-align: center;}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="text-center">
+                <img src="{{('construcao.png')}}" style="max-width:100%; height: auto" alt="manutencao">
+                <h1>Pagina em Construção</h1>
+                </p>Estamos providenciando conteudo!!!</p>
+        </div>
+    </body>
+</html>
