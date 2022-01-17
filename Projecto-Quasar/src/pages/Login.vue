a1fb81c76f1e0ddf8c99feca5a2ec7ed84821274
Antonio Xavier <antonio.75bi@gmail.com>
2022-01-17 15:52:39 +0200

    Meu Primeiro Codigo com Quasar

---
+++ Projecto-Quasar/src/pages/Login.vue
@@ -0,0 +1,16 @@
+
+<template>
+
+  <div class="col-0 col-md-6 flex justify-center content-center">
+  <h5>António Xavier</h5>
+  </div>
+   <div class="col-0 col-md-6 flex justify-center content-center">
+   <img src="~assets/Eu.jpg"  style="width: 200px; height: auto" class="responsive"
+    alt="login-image">
+   </div>
+</template>
+
+<script>
+
+
+</script>
